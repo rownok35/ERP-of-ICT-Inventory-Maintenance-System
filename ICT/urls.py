8539ea085668fb7de70739f1d9fab66cdc2e8ee2
rownok35 <rownokrezwan@gmail.com>
2022-12-14 02:30:35 +0600

    sidebar added

--- ICT/urls.py
+++ ICT/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('',include('profiles.urls')),
 
 ]
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
